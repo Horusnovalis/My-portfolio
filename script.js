08d8897634ef4ef5dff12ac1975896f56738fe55
Andy <biangawaruziandy24@gmail.com>
2026-07-31 10:41:34 +0200

    about-stats animation

--- script.js
+++ script.js
@@ -31,3 +31,48 @@ menu.addEventListener("click", function() {
         }
     }
 });
+
+const counters = document.querySelectorAll(".compteur");
+const aboutStats = document.querySelector(".about-stats");
+let counterStarted = false;
+let startCounter = () => {
+    if (counterStarted) return;
+    counterStarted = true;
+
+    counters.forEach ((counter) => {
+        const target = Number(counter.dataset.target);
+        let count = 0;
+        let duration = 2000;
+        const increment = target/(duration/16);
+        const updateCounter = () => {
+            count += increment;
+            if (count < target) {
+                counter.textContent = Math.ceil(count) + (target === 100 ? "%" : "+");
+                requestAnimationFrame(updateCounter);
+            } else {
+                counter.textContent = target + (target === 100 ? "%" : "+");
+            }
+        };
+        
+        updateCounter();
+    });
+};
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if(entry.isIntersecting) {
+                startCounter();
+
+                observer.unobserve(entry.target);
+            }
+        });
+    },
+    {
+        threshold: 0.5,
+    },
+);
+
+if(aboutStats) {
+    observer.observe(aboutStats);
+}
